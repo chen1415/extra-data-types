05e6e202005bdec51e7edf057fd33c4e0bcc7cb7
harvey_chen <harveychenwork@gmail.com>
2022-04-30 15:32:43 +0800

    change func name

--- src/email/index.ts
+++ src/email/index.ts
@@ -4,5 +4,5 @@ import { regEmailConfig } from "./config";
 export const isGmailType = (x: any): x is EmailType.GmailType =>
   regEmailConfig.gamilRegConfig.test(String(x).toLowerCase());
 
-export const isEmailType = (x: any): x is EmailType.StandardType =>
+export const isStandardEmailType = (x: any): x is EmailType.StandardType =>
   regEmailConfig.standardRegConfig.test(String(x).toLowerCase());
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,4 @@
 import type { EmailType } from "./email/index.d";
-import { isEmailType, isGmailType } from "./email";
+import { isStandardEmailType, isGmailType } from "./email";
 
-export { EmailType, isEmailType, isGmailType };
+export { EmailType, isStandardEmailType, isGmailType };
