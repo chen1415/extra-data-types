ebf3d0edb8166b6afb7fae5f2b7697335e610f15
IAmJerryJ <jerryjia1999429@gmail.com>
2023-02-22 14:39:34 +1100

    Add new date type

---
+++ src/date/config.ts
@@ -0,0 +1,3 @@
+export const regDateConfig = {
+  utcRegConfig: /\b[0-9]{4}-[0-9]{2}-[0-9]{2}t[0-9]{2}:[0-9]{2}:[0-9]{2}z\b/,
+};
---
+++ src/date/index.test.ts
@@ -0,0 +1,25 @@
+import { isUTCType } from './index';
+
+test('test utc type => true', () => {
+  expect(isUTCType('2014-08-29T06:44:03Z')).toBe(true);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29t06:44:03Z')).toBe(true);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29T06:44:03z')).toBe(true);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29a06:44:03z')).toBe(false);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('20140829t06:44:03z')).toBe(false);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29t064403z')).toBe(false);
+});
