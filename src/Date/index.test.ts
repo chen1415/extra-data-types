932b59aacc681d8017949da0a0729d775bb8f9e1
IAmJerryJ <jerryjia1999429@gmail.com>
2023-02-22 13:10:48 +1100

    Update readme and date types

--- src/Date/index.test.ts
+++ src/Date/index.test.ts
@@ -5,9 +5,21 @@ test('test utc type => true', () => {
 });
 
 test('test utc type => false', () => {
-  expect(isUTCType('2014-08-29t06:44:03Z')).toBe(false);
+  expect(isUTCType('2014-08-29t06:44:03Z')).toBe(true);
 });
 
 test('test utc type => false', () => {
-  expect(isUTCType('2014-08-29T06:44:03z')).toBe(false);
+  expect(isUTCType('2014-08-29T06:44:03z')).toBe(true);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29a06:44:03z')).toBe(false);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('20140829t06:44:03z')).toBe(false);
+});
+
+test('test utc type => false', () => {
+  expect(isUTCType('2014-08-29t064403z')).toBe(false);
 });
