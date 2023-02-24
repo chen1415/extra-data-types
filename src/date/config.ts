7a46ed92f5b476e6f31ce8f2649757139df0c588
IAmJerryJ <jerryjia1999429@gmail.com>
2023-02-24 15:26:04 +1100

    Update naming conventions

--- src/date/config.ts
+++ src/date/config.ts
@@ -1,4 +1,4 @@
 export const regDateConfig = {
-  YYYYMMDD_RegConfig: /\b[0-9]{4}-[0-9]{2}-[0-9]{2}\b/,
-  YYYYMMMDD_RegConfig: /\b[0-9]{4}-[A-Z]{3}-[0-9]{2}\b/,
+  YYYY_MM_DD_REGCONFIG: /\b[0-9]{4}-[0-9]{2}-[0-9]{2}\b/,
+  YYYY_MMM_DD_REGCONFIG: /\b[0-9]{4}-[A-Z]{3}-[0-9]{2}\b/,
 };
--- src/email/index.d.ts
+++ src/email/index.d.ts
@@ -1,7 +1,7 @@
 import { regEmailConfig } from './config';
 
 export declare namespace EmailSchema {
-  type Gmail_Type = typeof regEmailConfig.gamil_RegConfig;
-  type QQmail_Type = typeof regEmailConfig.qq_RegConfig;
-  type StandardEmail_Type = typeof regEmailConfig.standard_RegConfig;
+  type GMAIL_TYPE = typeof regEmailConfig.GMAIL_REGCONFIG;
+  type QQ_MAIL_TYPE = typeof regEmailConfig.QQ_REGCONFIG;
+  type STANDARD_EMAIL_TYPE = typeof regEmailConfig.STANDARD_REGCONFIG;
 }
