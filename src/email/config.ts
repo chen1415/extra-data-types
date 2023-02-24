c22c52d54fc5ff01bf127d570b6c2758f967e161
IAmJerryJ <jerryjia1999429@gmail.com>
2023-02-24 21:45:01 +1100

    Update Readme
    Update README.md and naming convention in config.ts

--- src/email/config.ts
+++ src/email/config.ts
@@ -1,6 +1,6 @@
 export const regEmailConfig = {
-  gamil_RegConfig: /^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/,
-  qq_RegConfig: /^[a-z0-9](\.?[a-z0-9]){5,}@qq\.com$/,
-  standard_RegConfig:
+  Gamil_RegConfig: /^[a-z0-9](\.?[a-z0-9]){5,}@g(oogle)?mail\.com$/,
+  QQ_RegConfig: /^[a-z0-9](\.?[a-z0-9]){5,}@qq\.com$/,
+  Standard_RegConfig:
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 };
--- src/email/index.ts
+++ src/email/index.ts
@@ -4,10 +4,10 @@ import { regEmailConfig } from './config';
 export * from './index.d';
 
 export const is_Gmail_Type = (x: unknown): x is EmailSchema.Gmail_Type =>
-  regEmailConfig.gamil_RegConfig.test(String(x).toLowerCase());
+  regEmailConfig.Gamil_RegConfig.test(String(x).toLowerCase());
 
 export const is_QQmail_Type = (x: unknown): x is EmailSchema.QQmail_Type =>
-  regEmailConfig.qq_RegConfig.test(String(x).toLowerCase());
+  regEmailConfig.QQ_RegConfig.test(String(x).toLowerCase());
 
 export const is_StandardEmail_Type = (x: unknown): x is EmailSchema.StandardEmail_Type =>
-  regEmailConfig.standard_RegConfig.test(String(x).toLowerCase());
+  regEmailConfig.Standard_RegConfig.test(String(x).toLowerCase());
